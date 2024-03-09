eb25d5bb97abed79f2d45e696c2327a0236d12fc
jlika <similika25@gmail.com>
2024-03-09 01:16:15 +0100

    initial core commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "@core/components";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: DashboardComponent}
+
+];
---
+++ src/app/core/services/index.ts
@@ -0,0 +1,5 @@
+import {LoaderService} from "@core/services/loader.service";
+
+export const services: any[] = [LoaderService]
+
+export * from './loader.service'
